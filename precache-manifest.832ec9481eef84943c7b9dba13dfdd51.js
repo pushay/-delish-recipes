3a3e483691b968341e81e398f4d3efbaca8bf7bf
pushay <sobocinskapraca@gmail.com>
2020-06-16 11:14:54 +0200

    Updates

---
+++ precache-manifest.832ec9481eef84943c7b9dba13dfdd51.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89325d25885d34e7c92e8e2046055042",
+    "url": "/delish-recipes/index.html"
+  },
+  {
+    "revision": "04e27de157b228597a24",
+    "url": "/delish-recipes/static/css/main.3c48fd1e.chunk.css"
+  },
+  {
+    "revision": "68344bb3795758141440",
+    "url": "/delish-recipes/static/js/2.a4ee3e43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/delish-recipes/static/js/2.a4ee3e43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e27de157b228597a24",
+    "url": "/delish-recipes/static/js/main.a211fbc6.chunk.js"
+  },
+  {
+    "revision": "c074e79fd317855340e0",
+    "url": "/delish-recipes/static/js/runtime-main.0b8bad24.js"
+  },
+  {
+    "revision": "8c894c27b1e7b7ed7d3a6f91275706c3",
+    "url": "/delish-recipes/static/media/dun.8c894c27.png"
+  },
+  {
+    "revision": "405da6d77def17c87a676ac7124033d8",
+    "url": "/delish-recipes/static/media/frog.405da6d7.png"
+  },
+  {
+    "revision": "e6ddce451eed3592e4ebf4ddd2ee6622",
+    "url": "/delish-recipes/static/media/fruits.e6ddce45.jpg"
+  },
+  {
+    "revision": "f775bfbadbdb6195286e6e1031ea7841",
+    "url": "/delish-recipes/static/media/green.f775bfba.png"
+  },
+  {
+    "revision": "e98672332da82ce420298abd4a0261f2",
+    "url": "/delish-recipes/static/media/restaurant.e9867233.jpg"
+  },
+  {
+    "revision": "598dea557eea1065d24fcca9ece7dccf",
+    "url": "/delish-recipes/static/media/story.598dea55.png"
+  }
+]);
